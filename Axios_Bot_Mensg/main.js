087f0c63fd7643c5c5f5274babaef647d882b14a
Caio Cordeiro <ccmr@cesar.school>
2019-10-04 16:12:30 -0300

    Ignorint token.js and Node modules

--- Axios_Bot_Mensg/main.js
+++ Axios_Bot_Mensg/main.js
@@ -1,2 +1,7 @@
 const {token_tel} = require('./token.js');
+const axios = require('axios');
+
+//https://api.telegram.org/botTOKEN/sendMessage?text="vacina%20da%20autismo"&chat_id=643618581
+msg = "DAAALEE"
+
 console.log(token_tel);
--- Axios_Bot_Mensg/token.js
+++
@@ -1,4 +0,0 @@
-const token_tel = "INSERT TOKEN HERE";
-module.exports={
-    token_tel:token_tel
-}
